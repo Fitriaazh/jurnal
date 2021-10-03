12c450d5c7392088b608035f32473f09815e3171
Fitriaazh <fitriaazizah529@gmail.com>
2021-10-03 08:03:24 +0700

    jurnal

---
+++ Jurnal.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Avatar from './Avatar.png';
+import Kalender from './Kalender.png';
+
+import "./jurnal.css";
+
+export default function jurnal() {
+    return (
+        <body>
+            <center>
+      <div className="home">
+      <i className="fas fa-home"></i></div>
+    <div className="diary">
+    <h1>Diary Journal</h1></div>
+    <p className="jadwal"> Yuk jadwalin kegiatan positif kamu..</p>
+
+    <div className="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Catatan
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea></label>
+      </div>
+        <div className="ava">
+        <img src={Avatar}alt="avatar"/>;
+        </div>
+
+<div className="kalender">
+<img src={Kalender}alt="kalender"/>;
+</div>
+
+    </center>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+    );
+}
